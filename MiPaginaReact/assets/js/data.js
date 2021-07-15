bd2d6dd7574583015d1cda723cb9dcc795ae3832
amphoeminix277 <31974692+amphoeminix277@users.noreply.github.com>
2021-07-15 10:02:51 -0500

    Add files via upload

---
+++ MiPaginaReact/assets/js/data.js
@@ -0,0 +1,79 @@
+const data = [
+  {
+    name: "Once",
+    description:
+      "Jane Hopper (nacida como Jane Ives), también conocida como Once, es uno de los personajes principales de la serie original de Netflix, Stranger Things. Ella tiene habilidades psicoquinéticas y además tiene un misterioso pasado, del cual muy pocos están enterados.",
+    imgUrl: "assets/imgs/animes/Once.jpg",
+  },
+  {
+    name: "Mike",
+    description:
+      "Mike es el segundo hijo de la familia Wheeler, que vive en Hawkins, Indiana junto a su madre, su padre y sus hermanas Nancy y Holly Wheeler. ... Mike era Mejor amigo de Lucas Sinclair y de Will Byers desde temprana edad. En el cuarto grado, Dustin Henderson llegó a su escuela y se unió a su grupo de amigos.",
+    imgUrl: "assets/imgs/animes/Mike.png",
+  },
+  {
+    name: "Dustin",
+    description: "Dustin Henderson es uno de los protagonistas de la serie Stranger Things, es interpretado por el actor Gaten Matarazzo. Es uno de los mejores amigos de Mike, Lucas y Will. ... Dustin es muy extrovertido, es gracioso y fantasioso. Siempre tiene un comentario gracioso que hacer.",
+    imgUrl: "assets/imgs/animes/Dustin.jpg",
+  },
+  {
+    name: "Will",
+    description:
+      "WILL BYERS, de doce años, es un niño dulce y sensible con problemas de identidad sexual. Recientemente se dio cuenta de que no encaja en la definición de 'normal' de la década de 1980, se puede ver Sus elecciones inocentes, como su ropa colorida, son una constante fuente de bullying.",
+    imgUrl: "assets/imgs/animes/Will.jpg",
+  },
+  {
+    name: "Lucas",
+    description:
+      "Lucas es un chico de 13 años de edad, es delgado y de estatura promedio a comparación de los otros chicos de su edad, tiene cabello corto de color negro y estilo afro corto y es de tez oscura. Tiene un carácter duro ademas de ser muy valiente y terco, es inteligente como sus amigos.",
+    imgUrl: "assets/imgs/animes/Lucas.jpg",
+  },
+  {
+    name: "Hopper",
+    description:
+      "Jim es un hombre alto, de pelo corto y castaño claro, con ojos color azul. Tiene aspecto de que no se preocupa mucho por como se ve. Siempre lleva unas pulceras azules en su muñeca, que en realidad eran las que usaba su hija para atarse el pelo.",
+    imgUrl: "assets/imgs/animes/Hopper.png",
+  },
+  {
+    name: "Joice",
+    description:
+      "Joyce Byers es uno de los personajes principales de la serie original de Netflix, Stranger Things. La mujer divorciada con poco dinero madre de Will y Jonathan Byers, que trabaja como vendedora al por menor y que llega a fin de mes gracias a su trabajo y al dinero que gana su hijo Jonathan.",
+    imgUrl: "assets/imgs/animes/Joice.jpg",
+  },
+  {
+    name: "Nancy",
+    description:
+      "Nancy es terca y combativa, pero también es amable, alegre e independiente con una moral muy fuerte. Además, parece preocuparse por los demás, como mostró cuando habló con Jonathan después de que su hermano Will hubiera desaparecido.",
+    imgUrl: "assets/imgs/animes/Nancy.png",
+  },
+  {
+    name: "Yonathan",
+    description:
+      "Jonathan Byers, interpretado por el miembro del reparto Charlie Heaton, es un personaje importante de Stranger Things. Es el hermano mayor de Will Byers. Su vida dio un vuelco tras la desaparición de su hermano menor.",
+    imgUrl: "assets/imgs/animes/Yonathan.jpg",
+  },
+  {
+    name: "Max",
+    description:
+      "Tiene una historia complicada y una relación difícil con su hermanastro, Billy. Ella también es buena en patineta, la cual utiliza para moverse por todos lados.",
+    imgUrl: "assets/imgs/animes/Max.jpg",
+  },
+  {
+    name: "Billy",
+    description:
+      "Personalidad y apariencia. De 17 años de edad, musculoso y confiado, tiene una historia complicada y una relación difícil con su padre y su hermanastra Max. Es cautivador y nervioso, con una naturaleza violenta e impredecible que sólo se revela a los más cercanos a él, especialmente a los más jóvenes.",
+    imgUrl: "assets/imgs/animes/Billy.jpg",
+  },
+  {
+    name: "Demogorgon",
+    description:
+      "Apariencia. El monstruo es alto y delgado con una figura humanoide. Su cabeza no tiene rasgos faciales y se abre como una flor para revelar nada más que dientes. Tiene pies y manos con garras, y su piel parece estar cubierta de líquidos.",
+    imgUrl: "assets/imgs/animes/Demogorgon.jpg",
+  },
+  {
+    name: "Stiv",
+    description:
+      "Steve Harrington es uno de los personajes principales de la serie original de Netflix, Stranger Things. Es un estudiante de la Universidad de Hawkins y ex novio de Nancy Wheeler. Le gusta el queso y la cerveza.",
+    imgUrl: "assets/imgs/animes/Stiv.jpg",
+  },
+];
